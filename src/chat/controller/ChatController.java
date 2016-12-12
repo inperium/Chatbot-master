a710bdd7852410a8b1ee27def00c5ad2b682829b
inperium <ibow9416@740s-pl-111.local>
2016-12-12 13:47:27 -0700

    Fixed error in making content null

---
+++ src/chat/controller/ChatController.java
@@ -0,0 +1,98 @@
+package chat.controller;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import chat.model.Chatbot;
+import chat.view.ChatFrame;
+import chat.view.ChatbotViewer;
+
+public class ChatController {
+	private Chatbot stupidBot;
+	private ChatbotViewer chatView;
+	private ChatFrame chatFrame;
+	public String questionList[] = { "What is the meaning of life?", "What do you like to do?", "Where do you live?" };
+
+	/**
+	 * Constructs Chatbot, chatView, and chatFrame.
+	 * 
+	 * @param The
+	 *            current user's input.
+	 * @return Whether or not the response contains twitter related characters.
+	 */
+
+	public ChatController() {
+		stupidBot = new Chatbot("Steve");
+		chatView = new ChatbotViewer();
+		chatFrame = new ChatFrame(this);
+	}
+
+	/**
+	 * Presents the user with a welcome message.
+	 */
+
+	public void start() {
+		this.chatFrame.getChatPanel().showMessage("Welcome to Chatbot!");
+	}
+
+	/**
+	 * Gets the Main Chatbot.
+	 * 
+	 * @return Main ChatBot
+	 */
+	public Chatbot getChatbot() {
+		return stupidBot;
+	}
+
+	/**
+	 * Runs the Chatbot checkers to see what it contains and formulate a
+	 * response.
+	 * 
+	 * @param The
+	 *            current user's input.
+	 * @return Whether or not the response contains something the Chatbot knows.
+	 */
+
+	public String useChatbotCheckers(String input) {
+		String answer = "";
+
+		if (!stupidBot.quitChecker(input)) {
+			if (stupidBot.contentChecker(input)) {
+				answer += "\nYou konw my special secret\n";
+			}
+			if (stupidBot.keyboardMashChecker(input)) {
+
+				answer += questionList[(int)(Math.random()*3)];
+
+			} else {
+				if (stupidBot.harambeChecker(input)) {
+					answer += "Harambe was innocent.\n";
+				} else {
+					if (stupidBot.memeChecker(input)) {
+						answer += "I can has memes?\n";
+					} else {
+						if (stupidBot.lengthChecker(input)) {
+							answer += "Sorry, I don't know about " + input + ".";
+						} else {
+							answer += "Sometimes silence is the answer.";
+						}
+					}
+				}
+			}
+		} else {		
+			chatView.displayMessage("Thank you for chatting!");
+
+			System.exit(0);
+		}
+		return answer;
+	}
+
+	/**
+	 * Gets the chatFrame.
+	 * 
+	 * @return The chatFrame.
+	 */
+
+	public ChatFrame getBaseFrame() {
+		return this.chatFrame;
+	}
+}
